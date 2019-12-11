ee65779fb3a4d589dba1f9f9b0cf38fbb0a86abd
Mario Jorge <mjribeiro@triscal.com.br>
2019-12-11 15:59:57 -0300

    JavaProj00

---
+++ JavaProj00/src/javaproj00/JavaProj00.java
@@ -0,0 +1,70 @@
+package javaproj00;
+
+import java.util.Scanner;
+
+/**
+ * @author mjribeiro
+ */
+public class JavaProj00 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*
+        int tempoTotal = 100;
+        Scanner x = new Scanner(System.in);
+        int time = x.nextInt();
+        int tempoIn = time;
+        double valorPagar = 50.00;
+        double valorMais = 2.00;
+        
+        if(tempoIn > tempoTotal){
+        valorPagar += (tempoIn-tempoTotal)*valorMais;
+        }
+        
+        System.out.println("Valor da conta: R$"+valorPagar);
+        */
+        
+        /*
+        int v = 0;
+        do{
+            Scanner x = new Scanner(System.in);
+            System.out.printf("Entre com dia da semana (1 à 7): ");
+            int a = x.nextInt();
+            int diaSem = a;
+            
+            v = a;
+            
+            switch(diaSem){
+                case 1:
+                    System.out.println("\nDia da semana: Domingo\n");
+                    break;
+                case 2:
+                    System.out.println("\nDia da semana: Segunda-Feira\n");
+                    break;
+                case 3:
+                    System.out.println("\nDia da semana: Terça-Feira\n");
+                    break;
+                case 4:
+                    System.out.println("\nDia da semana: Quarta-Feira\n");
+                    break;
+                case 5:
+                    System.out.println("\nDia da semana: Quinta-Feira\n");
+                    break;
+                case 6:
+                    System.out.println("\nDia da semana: Sexta-Feira\n");
+                    break;
+                case 7:
+                    System.out.println("\nDia da semana: Sabado\n");
+                    break;
+                default:
+                    System.out.println("\nDia da semana Inválido\n");
+                    break;
+            }
+        }while(v != 0);
+        */
+        
+    }
+    
+}
